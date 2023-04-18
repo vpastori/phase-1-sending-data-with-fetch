57a1493fecea3e9fb157cc272b933af19c32bfe4
Vincent Pastorino <vpastori@DESKTOP-TDG3FUD>
2023-04-17 22:40:01 -0400

    finished lab

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Add your code here
+function submitData(name, email) {
+    const formData = { name, email };
+  
+    const configObj = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify(formData)
+    };
+  
+    return fetch("http://localhost:3000/users", configObj)
+      .then(response => response.json())
+      .then(data => {
+        document.body.append(data.id);
+        return data;
+      })
+      .catch(error => {
+        document.body.append(error.message);
+        return error;
+      });
+  }
